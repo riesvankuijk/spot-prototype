667cba5670fe09355eeaf6cda740e25841f166b2
riesvankuijk <riesvankuijk@hotmail.com>
2026-02-08 14:35:27 +0000

    Add types for ffprobe-static

---
+++ types/vendor.d.ts
@@ -0,0 +1,9 @@
+declare module "ffprobe-static" {
+  const mod: { path: string };
+  export default mod;
+}
+
+declare module "ffmpeg-static" {
+  const mod: string | null;
+  export default mod;
+}
